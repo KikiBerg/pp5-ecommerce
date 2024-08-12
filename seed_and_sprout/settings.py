170beeff295dbc9411289fb1879a8abc50c1a8a3
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-08-12 17:49:41 +0000

    Add initial header, footer & content in base & index templates

--- seed_and_sprout/settings.py
+++ seed_and_sprout/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
 
+import os
 from pathlib import Path
+
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
