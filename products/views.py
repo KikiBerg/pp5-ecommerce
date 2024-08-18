28bd07a7285cea6bc428c886769abc9b3deeab5d
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-08-18 16:53:09 +0000

    Category filtering part 1: general categories

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 def all_products(request):
@@ -9,8 +9,19 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-
+    categories = None
+    is_gardening_supply = False
+    
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'gardening_supplies' in request.GET:
+            is_gardening_supply = True
+            products = products.filter(is_gardening_supply=True)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -23,6 +34,8 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
+        'is_gardening_supply': is_gardening_supply,
     }
 
     return render(request, 'products/products.html', context)
