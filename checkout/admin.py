a23c8909a3b5c579b5defcec9894c9094da3bbb2
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-08-30 16:01:38 +0000

    Profiles: added order history and updated checkout views

--- checkout/admin.py
+++ checkout/admin.py
@@ -10,8 +10,8 @@ class OrderLineItemAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'date',
-                       'delivery_cost', 'order_total',
+    readonly_fields = ('order_number', 'user_profile',
+                       'date', 'delivery_cost', 'order_total',
                        'grand_total', 'original_bag',
                        'stripe_pid')
 
