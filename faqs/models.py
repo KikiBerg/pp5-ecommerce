bf6600ddf6473970350a6f883ffb69f51f70d1af
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-08-31 16:15:03 +0000

    Faqs: define FAQ model

--- faqs/models.py
+++ faqs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from products.models import Category
 
-# Create your models here.
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='faqs')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['category', 'question']
+        verbose_name = 'FAQ'
+        verbose_name_plural = 'FAQs'
+
+    def __str__(self):
+        return self.question
