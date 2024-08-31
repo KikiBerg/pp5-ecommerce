14157d1790bc13c66fb25d2abbe37efc4605df5d
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-08-31 16:27:13 +0000

    Faqs: define initial view & wire up urls

--- faqs/views.py
+++ faqs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from products.models import Category
 
-# Create your views here.
+
+def faq_list(request):
+    categories = Category.objects.all()
+    return render(request, 'faqs/faq_list.html', {'categories': categories})
