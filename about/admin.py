34c2d27d668870f3f159f0fd76d45a9118233df5
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-09-02 13:53:04 +0000

    About: configure admin interface

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import About, Contact
 
-# Register your models here.
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    search_fields = ('name', 'email')
