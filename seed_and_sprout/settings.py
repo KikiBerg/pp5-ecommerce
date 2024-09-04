8916be410e90faf76b11c708c15a19d7c8ad68fa
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-09-04 15:17:22 +0000

    Deployment pt2: set secret key

--- seed_and_sprout/settings.py
+++ seed_and_sprout/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v=dvgwz07yg+j#j*8wndt+!(q3vx2dd90m6)$5@sa&q_tnwr3l'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-kikiberg-pp5ecommerce-2snke996bz6.ws.codeinstitute-ide.net',
